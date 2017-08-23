c2da0c1895c3dd39cdc7eda32368ab8d8a757546
Ngô Trường Đạt <ngotruongdat381@gmail.com>
2017-08-23 17:58:48 +0700

    update

--- html/main.jsx
+++ html/main.jsx
@@ -21,7 +21,7 @@ class Signin extends React.Component {
       })
       .then(function (response) {
         if(response.data == 'success'){
-          window.location.assign('http://localhost:7777/home')
+          window.location.assign('/home')
         }
       })
       .catch(function (error) {
